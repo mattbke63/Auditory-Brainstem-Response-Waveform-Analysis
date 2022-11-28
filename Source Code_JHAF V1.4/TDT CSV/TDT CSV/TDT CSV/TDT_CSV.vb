56c7420149e46396b358745c40c7381b23761ca1
mattbke63 <119333187+mattbke63@users.noreply.github.com>
2022-11-28 09:07:01 -0600

    Add files via upload

---
+++ Source Code_JHAF V1.4/TDT CSV/TDT CSV/TDT CSV/TDT_CSV.vb
@@ -0,0 +1,20 @@
+﻿Imports System.IO
+Public Class TDT_CSV
+    Dim Currentfile As String
+    Private Sub tdt_CSV_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'MsgBox("Good!")
+        loadfilename()
+        translatefile()
+        File.Create(CurDir() & "\Completed.f")
+        Close()
+    End Sub
+    Private Sub loadfilename()
+        Dim fPath = CurDir() & "\currentfile.f"
+        Dim afile As New IO.StreamReader(fPath, True)
+        Currentfile = afile.ReadLine
+    End Sub
+    Private Sub translatefile()
+        File.Delete(CurDir() & "\currentcsv.csv")
+        File.Copy(Currentfile, CurDir() & "\currentcsv.csv")
+    End Sub
+End Class
