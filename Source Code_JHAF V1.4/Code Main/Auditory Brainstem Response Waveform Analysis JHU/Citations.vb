46117eadec3e64120def56948603007e6d86c0ff
mattbke63 <119333187+mattbke63@users.noreply.github.com>
2022-11-28 09:16:38 -0600

    Add files via upload

---
+++ Source Code_JHAF V1.4/Code Main/Auditory Brainstem Response Waveform Analysis JHU/Citations.vb
@@ -0,0 +1,19 @@
+﻿Public Class Citations
+    Private Sub citations_Load(sender As Object, e As EventArgs) Handles Me.Load
+        loaddocumentnumber()
+
+    End Sub
+
+    Private Sub loaddocumentnumber()
+        If My.Computer.FileSystem.FileExists(CurDir() & "\Citation.txt") = True Then
+            Dim fPath = CurDir() & "\Citation.txt"
+            Dim afile As New IO.StreamReader(fPath, True)
+            TextBox1.Text = afile.ReadLine
+            'Label41.Text = afile.ReadLine
+        Else
+            TextBox1.Text = "Citation information not currently avalible"
+        End If
+    End Sub
+
+
+End Class
